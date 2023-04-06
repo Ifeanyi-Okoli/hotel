c0a1cedb3b25072b484d61eb3c36fd9e5747c4b0
Okoli Ifeanyi <Okoli.I@bullseye.local>
2023-04-06 01:12:21 +0100

    added images using Javascript

--- crowny/main.js
+++ crowny/main.js
@@ -1,37 +1,134 @@
-var rooms = [{
-    name:"A101",
-    id:0001,
+var rooms = [
+  {
+    name: "A101",
+    id: 0001,
     type: "single",
-    booked: false,
-    desc:'Man has to rest; all work and no play makes Rust a blunt dude',
-    image: "12/4/2023",
+    booked: true,
+    desc: "Man has to rest; all work and no play makes Rust a blunt dude",
+    image: "./assets/rm1.jpeg",
     size: "200 Square Feet",
-    price: "$100 per night"
-},
-{
-    name:"B201",
-    id:0002,
+    price: "$100 per night",
+  },
+  {
+    name: "B201",
+    id: 0002,
     type: "Double",
     booked: false,
-    desc:'Man has to rest; all work and no play makes Rust a blunt dude',
-    image: "12/4/2023",
+    desc: "Man has to rest; all work and no play makes Rust a blunt dude",
+    image: "./assets/rm2.jpeg",
     size: "200 Square Feet",
-    price: "$100 per night"
-},
-{
-    name:"E301",
-    id:0003,
+    price: "$100 per night",
+  },
+  {
+    name: "E301",
+    id: 0003,
     type: "Executive",
-    booked: false,
-    desc:'Man has to rest; all work and no play makes Rust a blunt dude',
-    image: "12/4/2023",
+    booked: true,
+    desc: "Man has to rest; all work and no play makes Rust a blunt dude",
+    image: "./assets/rm3.jpeg",
     size: "200 Square Feet",
-    price: "$100 per night"
-}
-]
+    price: "$100 per night",
+  },
+  {
+    name: "B201",
+    id: 0002,
+    type: "Double",
+    booked: false,
+    desc: "Man has to rest; all work and no play makes Rust a blunt dude",
+    image: "./assets/rm4.jpeg",
+    size: "200 Square Feet",
+    price: "$120 per night",
+  },
+];
 
-const panel = document.getElementById("panel");
+var board = document.getElementById("board");
 const btnAdd = document.getElementById("btnAdd");
 
-const toDo = document.getElementById("todo");
-const completed = document.getElementById("completed");
+const roomAdd = document.getElementById("rooms");
+const book = document.getElementById("book");
+
+roomAdd.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Rooms");
+  clearBoard();
+  createRoom(rooms);
+});
+
+book.addEventListener("click", (e) => {
+  e.preventDefault();
+  clearBoard();
+  var bookedRoom = rooms.filter((room) => room.booked === true);
+  createRoom(bookedRoom);
+});
+
+
+
+
+
+const createRoom = (rooms) => {
+  for (let room of rooms) {
+    const roomDiv = document.createElement("div");
+    // roomDiv.classList.add("room");
+    roomDiv.className =
+      "bg-white shadow-md flex items-center rounded-xl p-4 h-40";
+    // roomDiv.innerHTML = `room.name`;
+
+    const subDiv = document.createElement("div");
+    subDiv.className = "w-3/5 flex justify-start";
+
+    const imgDiv = document.createElement("div");
+    imgDiv.className = "w-3/5 flex justify-start";
+
+    var img = document.createElement("img");
+    img.src = room.image;
+    document.body.appendChild(img);
+
+    const list = document.createElement("ul");
+
+    const l1 = document.createElement("li");
+    l1.className = "font-bold text-gray-400";
+    l1.innerText = room.name;
+
+    const l2 = document.createElement("li");
+    l2.className = "font-extrabold text-slate-800 text-xl";
+    l2.innerText = room.price;
+
+    const l3 = document.createElement("li");
+    l3.className = "font-bold text-slate-400 text-xl";
+    l3.innerText = room.type;
+
+    const l4 = document.createElement("li");
+    l4.className = "font-extrabold text-xl";
+    l4.innerText = room.id;
+
+    const bookDiv = document.createElement("div");
+    bookDiv.className = "w-2/5 flex justify-end";
+
+    const bookStatus = document.createElement("div");
+    bookStatus.className = `p-2 rounded-full ${
+      room.booked ? "bg-green-500" : "bg-indigo-500"
+    }`;
+    bookStatus.innerText = `${room.booked ? "BOOKED" : "NOT BOOKED"}`;
+
+    bookDiv.appendChild(bookStatus);
+    list.appendChild(l1);
+    list.appendChild(l2);
+    list.appendChild(l3);
+    list.appendChild(l4);
+
+    roomDiv.appendChild(img);
+
+    subDiv.appendChild(list);
+    roomDiv.appendChild(imgDiv);
+    roomDiv.appendChild(subDiv);
+    roomDiv.appendChild(bookDiv);
+
+    board.appendChild(roomDiv);
+  }
+};
+
+const clearBoard = () => {
+  while (board.hasChildNodes()) {
+    board.removeChild(board.firstChild);
+  }
+};
