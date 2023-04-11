c36c28d701e206cc41b9b7fd2dd3fe25c1a3efdb
Okoli Ifeanyi <Okoli.I@bullseye.local>
2023-04-11 16:37:13 +0100

    Login page updated with background images

--- crowny/main.js
+++ crowny/main.js
@@ -1,47 +1,43 @@
+//introducing file system API
 // const fs = require("fs");
+
+//Array to hold the room objects
 var rooms = [
   {
     name: "A101",
-    // id: 0001,
-    // type: "single",
+    type: "single",
     booked: true,
     desc: "Man has to rest; all work and no play makes Rust a blunt dude",
-    image: "./assets/rm1.jpeg",
-    // size: "200 Square Feet",
-    // price: "$100 per night",
+    image: ["./assets/rm1.jpeg", "./assets/rm1a.jpeg", "./assets/rm1b.jpeg", "./assets/rm1c.jpeg"],
+    price: "$100 per night"
   },
   {
     name: "B201",
-    // id: 0002,
-    // type: "Double",
+    type: "Double",
     booked: false,
     desc: "Man has to rest; all work and no play makes Rust a blunt dude",
-    image: "./assets/rm2.jpeg",
-    // size: "200 Square Feet",
-    // price: "$100 per night",
+    image: ["./assets/rm2.jpeg", "./assets/rm2a.jpeg", "./assets/rm2b.jpeg", "./assets/rm2c.jpeg"],
+    price: "$100 per night",
   },
   {
     name: "E301",
-    // id: 0003,
-    // type: "Executive",
+    type: "Executive",
     booked: true,
     desc: "Man has to rest; all work and no play makes Rust a blunt dude",
-    image: "./assets/rm3.jpeg",
-    // size: "200 Square Feet",
-    // price: "$100 per night",
+    image: ["./assets/rm3.jpeg", "./assets/rm3a.jpeg", "./assets/rm3b.jpeg", "./assets/rm3c.jpeg"],
+    price: "$100 per night"
   },
   {
-    name: "B201",
-    // id: 0002,
-    // type: "Double",
+    name: "B101",
+    type: "Double",
     booked: false,
     desc: "Man has to rest; all work and no play makes Rust a blunt dude",
-    image: "./assets/rm4.jpeg",
-    // size: "200 Square Feet",
-    // price: "$120 per night",
+    image: ["./assets/rm4.jpeg", "./assets/rm4a.jpeg", "./assets/rm4b.jpeg", "./assets/rm4c.jpeg"],
+    price: "$120 per night"
   },
 ];
 
+//Applying the file system API and creating a JSON file
 // const jsonContent  = JSON.stringify(rooms);
 // fs.writeFile("./alphabet.json", jsonContent, "utf-8", (err) =>{
 //   if (err){
@@ -50,7 +46,7 @@ var rooms = [
 //   console.log("This file was saved!");
 // });
 
-
+//Declaring variables to get the various needed HTML elements
 var board = document.getElementById("board");
 const btnAdd = document.getElementById("btnAdd");
 const availableBtn = document.getElementById("available");
@@ -67,6 +63,7 @@ const bkdRooms = document.getElementById("bookedRooms");
 
 // const increaseRoom = document.getElementById("increaseRoom");
 
+//Function to clear the board of previous content
 const clearBoard = () => {
     while (board.hasChildNodes()) {
       board.removeChild(board.firstChild);
@@ -75,26 +72,28 @@ const clearBoard = () => {
 
 
 
-
+//Click event handler for adding rooms to the board that calls the clearBoard() and creatRoom() functions
 roomAdd.addEventListener("click", (e) => {
   e.preventDefault();
 //   console.log("Rooms");
-  clearBoard();
-  createRoom(rooms);
+  clearBoard(); //calling the clearBoard function on line #67
+  createRoom(rooms); //calling the createRoom function using rooms array as parameter
 });
 
+//Click event handler for adding only booked rooms to the board. 
 book.addEventListener("click", (e) => {
   e.preventDefault();
-  clearBoard();
-  var bookedRoom = rooms.filter((room) => room.booked === true);
-  createRoom(bookedRoom);
+  clearBoard(); //calling the clearBoard function on line #67
+  var bookedRoom = rooms.filter((room) => room.booked === true); //filtering the rooms array to get only the booked rooms
+  createRoom(bookedRoom); //calling createRoom function using bookedRoom as parameter.
 });
 
+//Click event handler for adding only available rooms to the board.
 availableBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  clearBoard();
-  var availBtn = rooms.filter((room) => room.booked === false);
-  createRoom(availBtn);
+  clearBoard(); //calling the clearBoard function on line #67
+  var availBtn = rooms.filter((room) => room.booked === false); //filtering the rooms array to get only the available rooms
+  createRoom(availBtn); //calling createRoom function for available rooms only using availBtn as parameter.
 });
 
 // btnAdd.addEventListener("click", (e)=>{
@@ -103,33 +102,39 @@ availableBtn.addEventListener("click", (e) => {
     // form.style.display = form.style.display==="none" ? "block" : "none";
 // })
 
+//Function to create the room cards using click event hnadler and forms
 formAdd.addEventListener("click", (e)=>{
     e.preventDefault();
-    var data = new FormData(form)
-    var title = data.get("title")
-    var desc = data.get("desc")
-    var booked = data.get("booked")
-    var date = data.get("date")
+    var data = new FormData(form); // create a FormData object
+    var title = data.get("title"); // get the value of the input field for room name
+    var desc = data.get("desc"); // get the value of the desc field
+    var booked = data.get("booked"); //get the value of the booked field.
+    var date = data.get("date"); //get the value of the date field.
+    var type = data.get("type"); //get the value of the type field
+    var price = data.get("price"); // get the value for the price field
 
-    if(title && desc){
-        var room = {name: title, desc: desc, booked:complete(booked), dueDate:date }
-        rooms.push(room);
-        form.classList.replace("border-red-500", "border-slate-200")
+    //condition to check if the form has values in the title and desc fields
+    if(title && date){
+        var room = {name: title, type: type, price: price, desc: desc, booked:complete(booked), dueDate:date }
+        rooms.push(room); // adds the object room to the rooms array.
+        form.classList.replace("border-red-500", "border-slate-200") //replaces the border color of the form to slate color
     }else{
-        form.classList +="border-red-500"
+        form.classList +="border-red-500"; //retains the border color as red if there is no enteries for title and date field.
     }
-    clearBoard();
-    createRoom(rooms);
+    clearBoard(); // calls the clearBoard fucntion of line #67
+    createRoom(rooms); // calls the createRoom function
 
-    updateMetrics();
-    showMetricBar();
-    alert(title, desc, booked, date)
+    updateMetrics(); //calls the updateMetrics function
+    showMetricBar(); //calls the showMetricBar function
+    alert(title, desc, booked, date); // alerts the user of the values entered in the form
 })
 
+//Function to check if the room is booked or not
 const complete=(v)=>{
     return v===null  ? false : true;
 }
 
+//Function to update the metrics of the rooms
 const updateMetrics = ()=>{
    var totalRooms = rooms.length;
    var bookedRooms = rooms.filter((room)=>room.booked===true).length;
@@ -140,6 +145,7 @@ const updateMetrics = ()=>{
    available.innerText = availableRooms;
 }
 
+//Function to show the metrics bar
 const showMetricBar=()=>{
     if(rooms.length > 1){
         topMetricBar.classList.replace("hidden", "block")
@@ -148,6 +154,7 @@ const showMetricBar=()=>{
     }
 }
 
+//Function to create the room cards
 const createRoom = (rooms) => {
   for (let room of rooms) {
     const roomDiv = document.createElement("div");
@@ -157,27 +164,49 @@ const createRoom = (rooms) => {
     // roomDiv.innerHTML = `room.name`;
 
     const subDiv = document.createElement("div");
-    subDiv.className = "w-3/5 flex justify-start";
+    subDiv.className = "w-auto flex justify-start";
 
     const imgDiv = document.createElement("div");
     imgDiv.className = "w-3/5 flex justify-start";
 
     var img = document.createElement("img");
-    img.src = room.image;
+    img.className = "border-2 border-white border-solid rounded-xl w-56 h-40 -pl-2 -ml-2"
+    delay();
+    function delayImage(ms){
+      return new Promise(resolve =>{
+        setTimeout(()=>{resolve("")}, ms);
+      })
+    }
+    async function delay(){
+    for(let i = 0; i < room.image.length; i++){
+      if(i >= room.image.length){
+        i = 0;
+      }
+        
+        img.src = room.image[i];
+        await delayImage(1000);
+    }
+  }
+    
+    // img.src = room.image;
     document.body.appendChild(img);
+
+
+    imgDiv.appendChild(img);
 
     const closeBtn = document.createElement("button");
     closeBtn.innerText = "+";
     closeBtn.className = "text-center text-2xl text-slate-700";
 
     const list = document.createElement("ul");
+    list.className = "-p-6 w-32"
 
     const l1 = document.createElement("li");
     l1.className = "font-bold text-gray-400";
     l1.innerText = room.name;
 
     const l2 = document.createElement("li");
-    l2.className = "font-extrabold text-slate-800 text-xl";
+    l2.className = "font-bold text-slate-800 text-md";
     l2.innerText = room.price;
 
     const l3 = document.createElement("li");
@@ -185,14 +214,14 @@ const createRoom = (rooms) => {
     l3.innerText = room.type;
 
     const l4 = document.createElement("li");
-    l4.className = "font-extrabold text-xl";
-    l4.innerText = room.id;
+    l4.className = "font-normal text-xs";
+    l4.innerText = room.desc;
 
     const bookDiv = document.createElement("div");
     bookDiv.className = "w-2/5 flex justify-end";
 
     const bookStatus = document.createElement("div");
-    bookStatus.className = `p-2 rounded-full ${
+    bookStatus.className = `p-2 rounded-full w-auto text-white text-xs font-bold ${
       room.booked ? "bg-green-500" : "bg-indigo-500"
     }`;
     bookStatus.innerText = `${room.booked ? "BOOKED" : "NOT BOOKED"}`;
@@ -205,6 +234,7 @@ const createRoom = (rooms) => {
 
     roomDiv.appendChild(img);
     roomDiv.appendChild(closeBtn);
+    roomDiv.appendChild(imgDiv);
 
     subDiv.appendChild(list);
     roomDiv.appendChild(imgDiv);
@@ -212,6 +242,7 @@ const createRoom = (rooms) => {
     roomDiv.appendChild(bookDiv);
 
     board.appendChild(roomDiv);
+    
   }
 };
 
@@ -224,20 +255,6 @@ const createRoom = (rooms) => {
 
 const increaseRoom = () =>{
     // e.preventDefault();
-    // rooms.push({
-    //     name: "A101",
-    //     // id: 0001,
-    //     // type: "single",
-    //     booked: true,
-    //     desc: "Man has to rest; all work and no play makes Rust a blunt dude",
-    //     image: "./assets/rm1.jpeg",
-    //     // size: "200 Square Feet",
-    //     // price: "$100 per night",
-    //   });
-    //   clearBoard();
-    //   createRoom(rooms);
-
-   
     // form.className.replace("hidden", "block")
     form.style.display = form.style.display==="none" ? "block" : "none";
 }
